3f5b2ab5ecfca759f605a3875d90f0525b3658c1
Anton Golubtsov <logrusadm@gmail.com>
2015-09-03 11:09:15 +0300

    Initial Commit

---
+++ move by exif/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  move by exif
+//
+//  Created by Anton U. Golubtsov on 03.09.15.
+//  Copyright (c) 2015 Anton U. Golubtsov. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
